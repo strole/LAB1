011e87dc046e46f4f3a62883792184e3796fdafb
Luka Štroliga <lstrol00@fesb.hr>
2021-11-08 18:08:07 +0100

    commit

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,14 @@ var express = require('express');
 var secured = require('../lib/middleware/secured');
 var router = express.Router();
 
-var allUsers = []
-
 /* GET user profile. */
 router.get('/user', secured(), function (req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
-  console.log("USER => ", req.user)
-  allUsers.push(req.user)
   res.render('user', {
     userProfile: JSON.stringify(userProfile, null, 2),
     title: 'Profile page',
-    userProfiles: allUsers
   });
 });
+
 
 module.exports = router;
